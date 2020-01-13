161e25fc43bae232c7a4a50cbd3be35f912ab7e0
coffeemori92 <coffeemori92@naver.com>
2020-01-13 23:44:17 +0900

    Are we done?

--- README.md
+++
@@ -1,3 +0,0 @@
-# Movie App 2019
-
-React JS Fundamental Course (2019 Update)
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   state = {
@@ -9,7 +10,6 @@ class App extends React.Component{
   };
   getMovies = async () => {
     const { data: { data: { movies } } } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    console.log(movies);
     this.setState({ movies, isLoading: false });
   }
   componentDidMount(){
@@ -18,20 +18,29 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? 'Loading' : movies.map(movie => {
-          console.log(movie);
-          return (
-              <Movie
-                key={movie.id} 
-                id={movie.id} 
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                poster={movie.medium_cover_image} /> 
-          ); 
+      <section className="container">
+        {isLoading ? 
+        (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => {
+              return (
+                  <Movie
+                    key={movie.id} 
+                    id={movie.id} 
+                    year={movie.year} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres} /> 
+                ); 
         })}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
